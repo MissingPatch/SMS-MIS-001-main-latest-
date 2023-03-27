916e507348cebe47fcd354298de192ec38ea4fd7
A̶k̶s̶h̶a̶z̶u̶r̶ ̶|̶ ̶d̶e̶v̶.̶ <92050792+Akshazz@users.noreply.github.com>
2023-03-28 04:59:35 +0800

    dep-

--- connection/connection.php
+++ connection/connection.php
@@ -4,10 +4,10 @@
 	}
 	function connection(){
 
-$host="217.21.88.1";
-$user="u476821515_SMS";
-$password="Bcpsms12@";
-$db="u476821515_SMS";  
+ $host="217.21.88.1";
+ $user="u476821515_SMS";
+ $password="Bcpsms12@";
+ $db="u476821515_SMS"; 
 
 
 $con = new mysqli($host, $user, $password, $db);
