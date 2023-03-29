df5bbd900633bd465091e5c97ab26c64effc156b
MissingPatch <114797102+MissingPatch@users.noreply.github.com>
2023-03-30 02:27:32 +0800

    last

--- empprofile_security.php
+++ empprofile_security.php
@@ -144,20 +144,48 @@ input[type="checkbox"]:checked + .slider:before {
 							
                       <!-- Old password -->
                       <div class="col-md-6">
+                      <div class="passwordContainer">
                         <label for="exampleInputPassword1" class="form-label">Current password *</label>
-                        <input type="password" name="pass" class="form-control" required>
+                        <input type="password" name="pass" class="form-control" id="password1" required>
+                        <i class="fa-solid fa-eye-slash" id="passwordIconId" style="margin-top:20px;"></i>
+                      </div>
                       </div>
                       <!-- New password -->
                       <div class="col-md-6">
+                      <div class="passwordContainer">
                       <label for="new-password" class="form-label">New password *</label>
                         <input type="password" name="newpass" class="form-control" id="new-password" required>
-                      
+                        <i class="fa-solid fa-eye-slash" id="passwordIconId" style="margin-top:20px;"></i>
+                      </div>
                       </div>
                       <!-- Confirm password -->
                       <div class="col-md-12">
+                      <div class="passwordContainer">
                       <label for="confirm-password" class="form-label">Confirm Password *</label>
                       <input type="password" class="form-control" name="confirmpass" id="confirm-password" required>
+                      <i class="fa-solid fa-eye-slash" id="passwordIconId" style="margin-top:20px;"></i>
                       <span id="password-match"></span>
+                      </div>
+                      <script
+                      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+                      integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+                      crossorigin="anonymous"
+                      ></script>
+
+                      <script>
+                      let showPassword = document.querySelector("#passwordIconId");
+                      const passwordField = document.querySelector("#password1");
+
+                      showPassword.addEventListener("click", function () {
+                        this.classList.toggle("fa-eye");
+
+                        const type =
+                          passwordField.getAttribute("type") === "pass"
+                            ? "text"
+                            : "pass";
+                        passwordField.setAttribute("type", type);
+                      });
+                      </script>
 
                       <script>
                       const newPasswordInput = document.getElementById("new-password");
