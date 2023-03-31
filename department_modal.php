e994af2b3d9dfdcc5e60adb952c3b9bd32258dd2
A̶k̶s̶h̶a̶z̶u̶r̶ ̶|̶ ̶d̶e̶v̶.̶ <92050792+Akshazz@users.noreply.github.com>
2023-03-31 23:46:52 +0800

    login

--- department_modal.php
+++ department_modal.php
@@ -1,5 +1,5 @@
 
-<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel1" aria-hidden="true">
   <div class="modal-dialog modal-sm">
     <div class="modal-content">
     <div class="modal-header">
